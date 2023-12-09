d10d3d34b178f85854d933a11659a819ac8b4519
Ridho Fahlevy <146181103+Fahlevvy@users.noreply.github.com>
2023-12-09 19:15:28 +0700

    P1 menambahkan contoh1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -2,7 +2,7 @@
 class Contoh1 extends CI_Controller
 {
     public function index()
-    {
+    {//pertemuan 1
         echo "<h1>Perkenalkan</h1>";
         echo"Nama saya Ridho Fahlevy 
             Saya tingga di daerah Bekasi
