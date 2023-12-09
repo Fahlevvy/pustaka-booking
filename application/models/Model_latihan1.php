2111db2d8b5605ac5bf870efbd1d0921b8f40d68
Ridho Fahlevy <146181103+Fahlevvy@users.noreply.github.com>
2023-12-09 19:50:16 +0700

    P2 membuat Model_latihan1.php

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -3,7 +3,7 @@ class Model_latihan1 extends CI_Model
 {
     //membuat variable untuk menampung nilai
     public $nilai1, $nilai2, $hasil;
-
+//Pertmuan2
     //method penjumlahan
     public function jumlah($nil1 = null, $nil2 = null)
     {
